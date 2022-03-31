66d2abc7c1a0adb345c144f9c3986238c134d53e
killinghail606 <zhurbiym@gmail.com>
2022-03-31 14:51:39 +0300

    trying to convert first block background to webp format again

--- src/components/index/Main.vue
+++ src/components/index/Main.vue
@@ -22,7 +22,7 @@
 
 <style lang='scss' scoped>
 .index-page-main {
-  background: url("/img/index-bg.png") center;
+  background: url("/img/index-bg.webp") center;
   background-size: cover;
   min-height: 500px;
   display: flex;
